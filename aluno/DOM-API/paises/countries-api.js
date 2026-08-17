4f7348b70ed43a39f7b5d3ae26441827130e2be6
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-08-17 16:47:27 -0300

    Feat: Atv 4 finalizada filtro aplicado para continente americano e africano

--- aluno/DOM-API/paises/countries-api.js
+++ aluno/DOM-API/paises/countries-api.js
@@ -12402,25 +12402,23 @@ const countries = [
 	}
 ];
 
+const continentes = ["América", "África"];
 const paises = document.querySelector(".nomePais")
-const Continentes = ["América", "África"]
 
+const paisesFiltrados = countries.filter(pais =>
+    continentes.includes(pais.localizacao.regiao.nome)
+);
 
-for(let i = 0; i < countries.length; i++){
+for(let i = 0; i < paisesFiltrados.length; i++){
 	let div = document.createElement('div')
 	let h3 = document.createElement('h3')
 	let img = document.createElement('img')
 
 	div.classList.add("container")
-
-	h3.textContent = countries[i].id["ISO-3166-1-ALPHA-2"] + " - "  + countries[i].nome.abreviado + " " + `(${countries[i].localizacao.regiao.nome})`
-	img.src = `https://flagcdn.com/w80/${countries[i].id["ISO-3166-1-ALPHA-2"].toLocaleLowerCase()}.png`
+	
+	h3.textContent = paisesFiltrados[i].id["ISO-3166-1-ALPHA-2"] + " - "  + paisesFiltrados[i].nome.abreviado + " " + `(${paisesFiltrados[i].localizacao.regiao.nome})`
+	img.src = `https://flagcdn.com/w80/${paisesFiltrados[i].id["ISO-3166-1-ALPHA-2"].toLocaleLowerCase()}.png`
 	div.appendChild(h3)
 	div.appendChild(img)
 	paises.appendChild(div)
 }
-
-// ## 4. Filtro de continentes
-
-// Adicione um array no topo do seu script com os valores `["América", "África"]`. Exiba apenas os países destes continentes.
-// Conseguindo, altere os continentes na lista para validar que está funcionando adequadamente.
